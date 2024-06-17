05bc5a9d0784836af4a6a64b9d6961f99bbac993
kimikoultramega <9379@holbertonstudents.com>
2024-06-17 20:22:49 +0000

    4-print_alphabt

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -13,15 +13,15 @@ int main(void)
 	char c;
 	/* Iteramos sobre todos los caracteres desde 'a' hasta 'z' */
 	for (c = 'a'; c <= 'z'; c++)
-{
+	{
 		/* Saltamos los caracteres 'q' y 'e' */
 		if (c == 'q' && c == 'e')
-{
+
 			continue;
-}
+
 		/* Imprimimos el carácter actual */
 		putchar(c);
-}
+	}
 	/* Imprimimos un salto de línea */
 	putchar('\n');
 	return (0);
