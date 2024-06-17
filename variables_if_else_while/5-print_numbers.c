fff0c5d580af06706199b5cf2a85d2d88dbe878c
kimikoultramega <9379@holbertonstudents.com>
2024-06-17 21:23:54 +0000

    5-print_numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Prints the alphabet in lowercase, except for q and e.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int i;/*Declaraci√n de mi variable*/
+
+	for (i = 0; i < 10; i++)
+{
+	printf("%d", i);
+}
+	printf("\n");
+
+	return (0);
+}
