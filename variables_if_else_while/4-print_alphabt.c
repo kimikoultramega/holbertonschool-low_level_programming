ab5ecefe45b3baaebb72f8d7f10ac87064a76389
kimikoultramega <9379@holbertonstudents.com>
2024-06-17 20:07:03 +0000

    4-print_alphabt

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+/**
+ * main - Entry point
+ *
+ * Description: This program assigns a random number to the variable n
+ * and prints the last digit of n with a description.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	/* Iteramos sobre todos los caracteres desde 'a' hasta 'z' */
+	for (char c = 'a'; c <= 'z'; c++)
+{
+		/* Saltamos los caracteres 'q' y 'e' */
+		if (c == 'q' || c == 'e')
+{
+			continue;
+}
+		/* Imprimimos el carácter actual */
+		putchar(c);
+}
+	/* Imprimimos un salto de línea */
+	putchar('\n');
+	return (0);
+}
