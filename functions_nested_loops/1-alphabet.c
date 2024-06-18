1bafaa4cdb2dba130ac5532fd83c8dcafdc79a0d
kimikoultramega <9379@holbertonstudents.com>
2024-06-18 22:00:37 +0000

    functions_nested_loops-1-alphabet.c

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,9 +1,14 @@
 #include "main.h"
-
+/**
+ * print_alphabet - imprime el abecedario en min
+ *
+ *
+ */
 void print_alphabet(void)
 {
 	char alpha;
-	for(alpha = 'a'; alpha <= 'z'; alpha++)
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
 	{
 		_putchar(alpha);
 	}
