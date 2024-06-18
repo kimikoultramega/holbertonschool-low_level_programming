5a2afd546458f5fdaba9211769293c8e9025c75d
kimikoultramega <9379@holbertonstudents.com>
2024-06-18 22:34:46 +0000

    2-print_alphabet_x10.c-functions_nested_loops

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - imprime x10 el alfabeto
+ *
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	char alpha;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
+		{
+			_putchar(alpha);
+		}
+		_putchar('\n');
+	}
+}
