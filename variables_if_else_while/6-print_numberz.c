0d92ef41c08761dcb0123a27f3dbe25eabd681d4
kimikoultramega <9379@holbertonstudents.com>
2024-06-18 13:00:57 +0000

    6-print_numberz

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -10,11 +10,11 @@ int main(void)
 {
 	int i;
 
-	for (i = 48; i < 58; i++)
+	for (i = 0; i < 10; i++)
 
 {
-	putchar(i);
-	putchar(10);
+	putchar((i % 10) + '0');
+	putchar('\n');
 }
 	return (0);
 }
