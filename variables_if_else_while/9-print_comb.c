522552c3ea3a0b8487ff51902933aba351125b55
kimikoultramega <9379@holbertonstudents.com>
2024-06-18 16:49:15 +0000

    9-print_comb

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints all numbers of base 16 in lowercase
+ * followed by a new line using only putchar
+ *
+ * Return: Always 0 (Success)
+ */
+
+
+int main(void)
+{
+	int num;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+	putchar(num + '0');
+	if (num != 9)
+	{
+		putchar(',');
+		putchar(' ');
+	}
+	}
+	putchar('\n');
+
+	return (0);
+
+}
