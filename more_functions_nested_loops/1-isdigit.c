a7463b37eaddd651764637440db208f76c3e3074
kimikoultramega <9379@holbertonstudents.com>
2024-06-19 01:16:22 +0000

    more_functions_nested_loops-1-isdigit.c

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isdigit - zzzzzzzzzzzzzzzz
+ * @c: zzzzzz
+ * Return: 1 o 0 zzzzzz
+ */
+int _isdigit(int c)
+{
+	if (c <= 57 && c >= 48)
+		return (1);
+	else
+		return (0);
+}
