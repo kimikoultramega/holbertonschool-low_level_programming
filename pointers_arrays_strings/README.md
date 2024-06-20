07022b1f677526d1fb1175d347bb36b7a4fa3937
kimikoultramega <9379@holbertonstudents.com>
2024-06-20 19:33:02 +0000

    0-reset_to_98.c

---
+++ pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Stings, pointers and arrays
---
+++ pointers_arrays_strings/test/main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+int main(void) {
+	int  resultado;
+
+
+	printf("hola\n");
+	resultado = add(3, 2);
+	printf("resultado = %d\n", resultado);
+	resultado = add(3232334123, 100);
+	printf("resutlado = %d\n", resultado);
+
+
+	return 0;
+}
