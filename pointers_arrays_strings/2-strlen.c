ea80d00f1e2e6e79ec592d2cd98058c712f11e7b
kimikoultramega <9379@holbertonstudents.com>
2024-06-22 22:28:31 +0000

    2-strlen.c

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strlen - cuenta
+ *
+ * @s: puntero hacia una variable del tipo char
+ *
+ * Return: Devuelve la longitud del string.
+ */
+
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	for (len = 0; s[len] != '\0'; len++)
+	{
+
+	}
+
+	return (len);
+}
