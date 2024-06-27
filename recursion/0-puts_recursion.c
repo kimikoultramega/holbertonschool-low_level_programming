67fc325311e81ba51b21f20671c45cec69b3dc48
kimikoultramega <9379@holbertonstudents.com>
2024-06-27 23:57:51 +0000

    0-puts_recursion.c

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -2,20 +2,20 @@
 #include <stdio.h>
 
 /**
- *
- *
+ * _puts_recursion - recursion
+ * @s: puntero
  *
  *
  */
 
 void _puts_recursion(char *s)
 {
-	if(*s == '\0')
+	if (*s == '\0')
 	{
 		_putchar('\n');
 		return;
 	}
-	
+
 	_putchar(*s);
 	_puts_recursion(s + 1);
 }
