f17115d168d5a2bf233959c118ebfaeb174d8a66
kimikoultramega <9379@holbertonstudents.com>
2024-06-29 22:29:01 +0000

    3-factorial.c

---
+++ recursion/3-factorial.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * factorial - factorial
+ * @n: variable
+ * Return: factorial
+ *
+ *
+ */
+
+
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	if (n == 0)
+	{
+		return (1);
+	}
+	return (n * factorial(n - 1));
+}
