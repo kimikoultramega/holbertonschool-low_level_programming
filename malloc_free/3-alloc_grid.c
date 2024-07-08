b98ba58e0d23c09eb96b802b05f1c863744bc077
kimikoultramega <9379@holbertonstudents.com>
2024-07-08 13:44:52 +0000

    3-alloc_grid.c

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -3,28 +3,46 @@
 #include <stdlib.h>
 
 /**
- *
- *
- *
- *
- *
+ * alloc_grid - alloc grid
+ * @width: width
+ * @height: height
+ * Return: 0
  */
 
 int **alloc_grid(int width, int height)
 {
 
-	int **biblioteca;
-	int i, j;
+	int **grid;
+	int i;
+	int j;
 
 
 	if (width <= 0 || height <= 0)
 	{
 		return (NULL);
 	}
-	
-	biblioteca = (int **)malloc(height * sizeof(int *));
+
+	grid = (int **)malloc(height * sizeof(int *));
 
 	if (grid == NULL)
 	{
 		return (NULL);
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = malloc(width * sizeof(int));
+		if (grid[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+			{
+				free(grid[j]);
+			}
+			free(grid);
+		}
+	}
+
+	return (NULL);
 }
+
+
