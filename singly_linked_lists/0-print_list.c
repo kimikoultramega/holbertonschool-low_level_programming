7fc54549f98fdaa4a29938bdd11e0c59e12d710c
kimikoultramega <9379@holbertonstudents.com>
2024-08-02 21:37:29 +0000

    me tenes re quemado

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,56 +1,28 @@
-#include <stdio.h>
-<<<<<<< HEAD
-#include <stddef.h>
 #include "lists.h"
+#include <stdio.h>
+#include <stddef.h>
 /**
- * print_list - funcion
- * @h: puntero a struct
- * Return: nono_count
+ * print_list - lista
+ * list_t - lista
+ * @h: puntero a header
+ * Return: cantidad de nodos
  */
 size_t print_list(const list_t *h)
 {
 	size_t nono_count = 0;
-=======
-#include "lists.h"
-
-/**
- * print_list - linked list
- * @h: puntero de la funcion.
- * Return: nonocounter
- *
- *
- */
-
-size_t print_list(const list_t *h)
-{
-	size_t nonocounter = 0;
->>>>>>> dc43786673cd4d218ed87a3301e0f698ad17b5ed
 
 	while (h != NULL)
 	{
 		if (h->str == NULL)
 		{
-<<<<<<< HEAD
 			printf("[0] (nil)\n");
 		}
 		else
 		{
-			printf("%i %s", h->len, h->str);
+			printf("[%i] %s\n", h->len, h->str);
 		}
-		h = h->next;
 		nono_count++;
+		h = h->next;
 	}
 	return (nono_count);
-=======
-		printf("[0] (nil)\n");
-		}
-		else
-		{
-			printf("[unsigned int] char *, h->len, h->str");
-		}
-		nonocounter ++;
-		h = h->next;
-	}
-	return (nonocounter);
->>>>>>> dc43786673cd4d218ed87a3301e0f698ad17b5ed
 }
