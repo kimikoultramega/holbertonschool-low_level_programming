8d7d3299034e16cc60b5ebd151ece4a9e4adc44b
kimikoultramega <9379@holbertonstudents.com>
2024-08-02 17:53:06 +0000

    0-print_list.c

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - linked list
+ * @h: puntero de la funcion.
+ * Return: nonocounter
+ *
+ *
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t nonocounter = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+		printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[unsigned int] char *, h->len, h->str");
+		}
+	}
+	return (nonocounter);
+}
--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,4 +1,24 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stddef.h>
 
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct lists_s
+{
+	char *str;
+	unsigned int len;
+	struct lists_s *next;
+} list_t;
+
+/* Prototipos de funciones */
+size_t print_list(const list_t *h);
+
+#endif /* LISTS_H */
